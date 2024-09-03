b44dbabbc8729090f0feded1c2eb9e18714839ce
alv0804 <Muhammadalvinapriliandi@gmail.com>
2024-09-03 12:34:23 +0700

    Perkuliahan 1

---
+++ 1_intro.py
@@ -0,0 +1,17 @@
+# deklarasi variabel
+a = 20
+b = 10
+c = 50
+d = a * b / b * c
+# Operasi (Operation) -> tambah (+), Kurang (-), bagi (/), kali(*), modul(%), pangkat(**)
+perkalian = a * b
+pembagian = a / b 
+pengurangan = a - b
+module = a % b 
+pangkat = a ** b
+print(perkalian)
+print(perkalian)
+print(pengurangan)
+print(module)
+print(pangkat)
+print("kelas king")
